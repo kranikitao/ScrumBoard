b2ff5bc238cdf6a5395edcb0e17442cb87868625
Nikita Krasnoyartsev <i@knone.ru>
2015-03-04 20:52:24 +0300

    test: return active tasks

--- src/ScrumBoard/Infrastructure/Delivery/ScrumBoardBundle/Tests/Controller/TaskControllerTest.php
+++ src/ScrumBoard/Infrastructure/Delivery/ScrumBoardBundle/Tests/Controller/TaskControllerTest.php
@@ -6,13 +6,33 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class TaskControllerTest extends WebTestCase
 {
-    public function test_SendRequestToTask_ShouldReturn200OkAndNoTasks()
+    /**
+     * @test
+     */
+    public function sendRequestToTask_ShouldReturn200OkAndNoTasks()
     {
-        $client = static::createClient();
+        $client = static::createClient(['environment' => 'test']);
 
-        $crawler = $client->request('GET', '/task/', []);
+        $crawler = $client->request('GET', '/task/');
 
         $this->assertEquals($client->getResponse()->getStatusCode(), 200);
         $this->assertTrue($crawler->filter('html>body>ul>li')->count() === 0);
+    }
+
+    /**
+     * @test
+     */
+    public function sendRequestToTask_ShouldReturnActiveTasks()
+    {
+
+        $client = static::createClient(['environment' => 'test']);
+
+        //todo: загрузить фикстуры
+        $kernel = $client->getKernel();
+
+        $crawler = $client->request('GET', '/task/');
+
+        $this->assertEquals($client->getResponse()->getStatusCode(), 200);
+        $this->assertEquals(1, $crawler->filter('html>body>ul>li>a')->count());
     }
 }
